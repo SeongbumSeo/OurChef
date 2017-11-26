e29ef73e2a376b663b915dae2fc9aaef2a881ac6
SeongbumSeo <sBum.Seo@gmail.com>
2017-11-26 23:49:42 +0900

    Ingredient

---
+++ src/Data/IngredientList.java
@@ -0,0 +1,23 @@
+package Data;
+
+import java.util.*;
+import Main.*;
+
+public class IngredientList {
+	private List<Ingredient> ingredients;
+	
+	public IngredientList() {
+		ingredients = new ArrayList<Ingredient>();
+	}
+	
+	public void load(String filename) {
+		List<String[]> data = DataManager.readCSV(filename);
+		Iterator<String[]> itr = data.iterator();
+		String[] row;
+		
+		while (itr.hasNext()) {
+			row = itr.next();
+			ingredients.add(new Ingredient(Integer.parseInt(row[0]), row[1], row[2]));
+		}
+	}
+}
