134bf409d98b5be91497a2ec07ea59bf76a4d260
SeongbumSeo <sBum.Seo@gmail.com>
2017-11-30 12:21:50 +0900

    ImageButton 주석 처리

--- src/GUI/ImageButton.java
+++ src/GUI/ImageButton.java
@@ -6,16 +6,27 @@ import javax.swing.*;
 
 public class ImageButton extends JButton
 {
-	private ImageIcon image;
-	private ImageIcon hoverImage;
-	private ImageButtonListener imageButtonL;
-	private int iX, iY;
+	private ImageIcon image; // 버튼 이미지
+	private ImageIcon hoverImage; // 마우스 오버 시 버튼 이미지
+	private int iX, iY; // 버튼의 위치
 	
+	private ImageButtonListener imageButtonL;
+	
+	/**
+	 * 이미지 버튼의 생성자입니다.
+	 * @param imageName 버튼 이미지
+	 */
 	public ImageButton(String imageName) {
 		image = new ImageIcon(imageName);
 		hoverImage = null;
 		constructor();
 	}
+	/**
+	 * 이미지 버튼의 생성자입니다.
+	 * @param imageName 버튼 이미지
+	 * @param x 버튼의 위치 X
+	 * @param y 버튼의 위치 Y
+	 */
 	public ImageButton(String imageName, int x, int y) {
 		iX = x;
 		iY = y;
@@ -24,11 +35,23 @@ public class ImageButton extends JButton
 		hoverImage = null;
 		constructor();
 	}
+	/**
+	 * 이미지 버튼의 생성자입니다.
+	 * @param imageName 버튼 이미지
+	 * @param hoverImageName 마우스 오버 시 버튼 이미지
+	 */
 	public ImageButton(String imageName, String hoverImageName) {
 		image = new ImageIcon(imageName);
 		hoverImage = new ImageIcon(hoverImageName);
 		constructor();
 	}
+	/**
+	 * 이미지 버튼의 생성자입니다.
+	 * @param imageName 버튼 이미지
+	 * @param hoverImageName 마우스 오버 시 버튼 이미지
+	 * @param x 버튼의 위치 X
+	 * @param y 버튼의 위치 Y
+	 */
 	public ImageButton(String imageName, String hoverImageName, int x, int y) {
 		iX = x;
 		iY = y;
@@ -38,35 +61,49 @@ public class ImageButton extends JButton
 		constructor();
 	}
 	
+	/**
+	 * 버튼의 생성 및 초기값 설정에 직접 관여하는 메소드입니다.
+	 */
 	private void constructor() {
 		imageButtonL = new ImageButtonListener();
 
-		this.setSize(new Dimension(image.getIconWidth(), image.getIconHeight()));
 		this.setContentAreaFilled(false); // 버튼 바탕색 제거
 		this.setBorderPainted(false); // 버튼 테두리 제거
 		this.setIcon(image); // 이미지 적용
+		this.setSize(new Dimension(image.getIconWidth(), image.getIconHeight())); // 크기 설정
+		this.setLocation(iX, iY); // 위치 설정
 		this.addMouseListener(imageButtonL);
-		this.setLocation(iX, iY);
 	}
 	
-	
+	/**
+	 * 이미지 버튼의 마우스 이벤트 리스너입니다.
+	 */
 	private class ImageButtonListener implements MouseListener
 	{
 		public void mouseEntered(MouseEvent event) {
+			// 이미지 변경
 			if (hoverImage == null)
 				return;
 			((JButton)event.getSource()).setIcon(hoverImage);
+			
+			// 크기 재설정
 			setSize(new Dimension(hoverImage.getIconWidth(), hoverImage.getIconHeight()));
 			
+			// 위치 재설정
 			setLocation(iX - (hoverImage.getIconWidth() - image.getIconWidth())/2,
 						iY - (hoverImage.getIconHeight() - image.getIconHeight())/2);
 		}
 		
 		public void mouseExited(MouseEvent event) {
+			// 이미지 변경
 			if (image == null)
 				return;
 			((JButton)event.getSource()).setIcon(image);
+			
+			// 크기 재설정
 			setSize(new Dimension(image.getIconWidth(), image.getIconHeight()));
+			
+			// 위치 재설정
 			setLocation(iX, iY);
 		}
 		
