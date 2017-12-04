415b2c92ba7d5e46177a68d6df6b33897a087436
deanKWON <33994508+deanKWON@users.noreply.github.com>
2017-12-04 23:55:18 +0900

    예진이 코드

--- src/Main/SoundManager.java
+++ src/Main/SoundManager.java
@@ -49,20 +49,3 @@ public class SoundManager extends Thread {
 	}
 
 }
-
-
-
-
-
-
-//예진이 코드
-//public class SoundManager {
-//	private static Sound mSound;
-//	
-//	public static void playBackground() {
-//		File clap = new File("./sounds/background.mp3");
-//		mSound = new Sound(clap);
-//		mSound.playLoop();
-//	}
-//	
-//}
