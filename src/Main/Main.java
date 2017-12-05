5c2eaae258628eaea2f8d4994ffc7109486960a6
SeongbumSeo <sBum.Seo@gmail.com>
2017-12-05 22:04:43 +0900

    Main 클래스 주석

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -9,9 +9,9 @@ public class Main
 	 * @param args 실행 인자
 	 */
 	public static void main(String[] args) {
-		DataManager.loadData();
-		SceneManager.showWindow("Our Chef", 1600, 900);
-		SceneManager.switchScene(new IntroScene());
-		SoundManager.playBackground();
+		DataManager.loadData(); // 프로그램 구동에 필요한 데이터 로드
+		SceneManager.showWindow("Our Chef", 1600, 900); // 윈도우 보여주기
+		SceneManager.switchScene(new IntroScene()); // IntroScene으로 씬 전환
+		SoundManager.playBackground(); // BGM 재생
 	}
 }
