710c53bc0ac16870bbe22429bac91172cf0ee9ef
SeongbumSeo <sBum.Seo@gmail.com>
2017-12-09 23:01:38 +0900

    미사용 변수 삭제

--- src/Main/DataManager.java
+++ src/Main/DataManager.java
@@ -9,7 +9,6 @@ public class DataManager
 	private static List<Ingredient> ingredients; // 전체 재료
 	private static List<Ingredient> cart; // 카트 내 재료
 	private static List<Recipe> recipes; // 전체 레시피
-	private static List<Recipe> searchedRecipes; // 탐색된 레시피
 	
 	/**
 	 * 프로그램을 구동하는데 필요한 모든 데이터를 불러오는 메소드입니다.
@@ -24,5 +23,4 @@ public class DataManager
 	public static List<Ingredient> getIngredients() { return ingredients; }
 	public static List<Ingredient> getCart() { return cart; }
 	public static List<Recipe> getRecipes() { return recipes; }
-	public static List<Recipe> getSearchedRecipes() { return searchedRecipes; }
 }
