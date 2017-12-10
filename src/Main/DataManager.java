8ad19287a64e2415fe4d2a547e1c6e3fb136d71f
SeongbumSeo <sBum.Seo@gmail.com>
2017-12-10 09:19:03 +0900

    Main.DataManager 주석

--- src/Main/DataManager.java
+++ src/Main/DataManager.java
@@ -14,10 +14,10 @@ public class DataManager
 	 * 프로그램을 구동하는데 필요한 모든 데이터를 불러오는 메소드입니다.
 	 */
 	public static void loadData() {
-		ingredients = Data.Ingredient.load("data/ingredients.csv");
-		recipes = Data.Recipe.load("data/recipes.csv", ingredients);
-		Data.Slide.load("data/slides.csv", recipes);
-		cart = new ArrayList<Ingredient>();
+		ingredients = Data.Ingredient.load("data/ingredients.csv"); // 전체 재료 데이터 로드
+		recipes = Data.Recipe.load("data/recipes.csv", ingredients); // 전체 레시피 데이터 로드
+		Data.Slide.load("data/slides.csv", recipes); // 레시피 별 슬라이드 데이터 로드
+		cart = new ArrayList<Ingredient>(); // 카트 리스트 선언
 	}
 	
 	public static List<Ingredient> getIngredients() { return ingredients; }
