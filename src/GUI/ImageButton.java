2eaaac5bcb2459ae3f2218c1f4e398638f5a6097
SeongbumSeo <sBum.Seo@gmail.com>
2017-12-10 09:01:38 +0900

    GUI.ImageButton 주석

--- src/GUI/ImageButton.java
+++ src/GUI/ImageButton.java
@@ -8,63 +8,62 @@ import Main.*;
 public class ImageButton extends JButton
 {
 	private ImageIcon image; // 버튼 이미지
-	private ImageIcon hoverImage; // 마우스 오버 시 버튼 이미지
-	private int iX, iY; // 버튼의 위치
+	private ImageIcon hoverImage; // 마우스오버 시 버튼 이미지
+	private int iX, iY; // 버튼의 생성 위치
 	private boolean onSound;
 	
-	private ImageButtonListener imageButtonL;
+	private ImageButtonListener imageButtonL; // 마우스 이벤트 리스너
 	
 	/**
 	 * 이미지 버튼의 생성자입니다.
 	 * @param imageName 버튼 이미지
 	 */
 	public ImageButton(String imageName) {
-		image = new ImageIcon(imageName);
-		hoverImage = null;
+		image = new ImageIcon(imageName); // 이미지 아이콘 객체
+		hoverImage = null; // 마우스오버 이미지 없음
 		onSound = false;
-		constructor();
+		init(); // 이미지버튼 초기화
 	}
 	/**
 	 * 이미지 버튼의 생성자입니다.
 	 * @param imageName 버튼 이미지
-	 * @param x 버튼의 위치 X
-	 * @param y 버튼의 위치 Y
+	 * @param x 버튼의 생성 위치 X
+	 * @param y 버튼의 생성 위치 Y
 	 */
 	public ImageButton(String imageName, int x, int y) {
-		iX = x;
-		iY = y;
+		iX = x; // 버튼의 생성 위치 X
+		iY = y; // 버튼의 생성 위치 Y
 		
-		image = new ImageIcon(imageName);
+		image = new ImageIcon(imageName); // 이미지 아이콘 객체
 		hoverImage = null;
-		constructor();
+		init(); // 이미지버튼 초기화
 	}
-	
 	/**
 	 * 이미지 버튼의 생성자입니다.
 	 * @param imageName 버튼 이미지
 	 * @param hoverImageName 마우스 오버 시 버튼 이미지
 	 */
 	public ImageButton(String imageName, String hoverImageName) {
-		image = new ImageIcon(imageName);
-		hoverImage = new ImageIcon(hoverImageName);
+		image = new ImageIcon(imageName); // 이미지 아이콘 객체
+		hoverImage = new ImageIcon(hoverImageName); // 마우스오버 시 적용할 이미지 아이콘 객체
 		onSound = true;
-		constructor();
+		init(); // 이미지버튼 초기화
 	}
 	/**
 	 * 이미지 버튼의 생성자입니다.
 	 * @param imageName 버튼 이미지
 	 * @param hoverImageName 마우스 오버 시 버튼 이미지
-	 * @param x 버튼의 위치 X
-	 * @param y 버튼의 위치 Y
+	 * @param x 버튼의 생성 위치 X
+	 * @param y 버튼의 생성 위치 Y
 	 */
 	public ImageButton(String imageName, String hoverImageName, int x, int y) {
-		iX = x;
-		iY = y;
-		
-		image = new ImageIcon(imageName);
-		hoverImage = new ImageIcon(hoverImageName);
+		iX = x; // 버튼의 생성 위치 X
+		iY = y; // 버튼의 생성 위치 Y
+
+		image = new ImageIcon(imageName); // 이미지 아이콘 객체
+		hoverImage = new ImageIcon(hoverImageName); // 마우스오버 시 적용할 이미지 아이콘 객체
 		onSound = true;
-		constructor();
+		init(); // 이미지버튼 초기화
 	}
 	
 	public void SoundOff() {
@@ -74,11 +73,10 @@ public class ImageButton extends JButton
 		onSound = true;
 	}
 	
-	
 	/**
 	 * 버튼의 생성 및 초기값 설정에 직접 관여하는 메소드입니다.
 	 */
-	private void constructor() {
+	private void init() {
 		imageButtonL = new ImageButtonListener();
 
 		this.setContentAreaFilled(false); // 버튼 바탕색 제거
@@ -86,7 +84,7 @@ public class ImageButton extends JButton
 		this.setIcon(image); // 이미지 적용
 		this.setSize(new Dimension(image.getIconWidth(), image.getIconHeight())); // 크기 설정
 		this.setLocation(iX, iY); // 위치 설정
-		this.addMouseListener(imageButtonL);
+		this.addMouseListener(imageButtonL); // 마우스 이벤트 리스너 설정
 	}
 	
 	/**
