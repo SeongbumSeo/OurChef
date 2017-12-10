196b82924b1f6a11a11d293403e4dc42f8523622
SeongbumSeo <sBum.Seo@gmail.com>
2017-12-10 09:22:22 +0900

    Main.IOManager 주석

--- src/Main/IOManager.java
+++ src/Main/IOManager.java
@@ -6,10 +6,16 @@ import com.opencsv.*;
 
 public class IOManager
 {
+	/**
+	 * CSV 파일을 읽어 문자열 배열의 리스트로 반환합니다.
+	 * @param filename 파일명
+	 * @return 문자열 배열의 리스트
+	 */
 	public static List<String[]> readCSV(String filename) {
 		List<String[]> data = new ArrayList<String[]>();
 		
 		try {
+			// CSV 파일을 UTF-8 인코딩으로 오픈합니다.
 			CSVReader reader = new CSVReader(new InputStreamReader(new FileInputStream(filename), "UTF-8"));
 			
 			String[] str;
